1a2848b9c912344499381a8e2eedb041016d0e8a
prodws <169688005+prodws@users.noreply.github.com>
2026-08-03 17:45:29 +0200

    Update debris to have rotation, delayed destruction and effect support

--- Assets/Scripts/Debris.cs
+++ Assets/Scripts/Debris.cs
@@ -3,25 +3,39 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Debris : MonoBehaviour
 {
-    [SerializeField] private int numberOfHits = 1;
+    [SerializeField] private int numberOfHits = 3;
     [SerializeField] private Rigidbody rb;
-    [SerializeField] private float hitForce = 2f;
+    [SerializeField] private float hitTorque = 3f;
+    [SerializeField] private float hitForce = 1.5f;
+    [SerializeField] private GameObject destroyEffect;
+    [SerializeField] private float destroyDelay = 0.2f;
+    private bool destroyed;
+
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.angularDamping = 8f;
         rb.isKinematic = true;
     }
 
     public void Hit(Vector3 direction)
     {
+        if (destroyed) return;
+
         rb.isKinematic = false;
         rb.AddForce(direction * hitForce, ForceMode.Impulse);
+        rb.AddTorque(Random.insideUnitSphere * hitTorque, ForceMode.Impulse);
+
 
         if (--numberOfHits <= 0)
         {
-            Destroy(gameObject);
+            if (destroyEffect != null)
+                Instantiate(destroyEffect, transform.position, Quaternion.identity);
+            destroyed = true;
+
+            Destroy(gameObject, destroyDelay);
             Debug.Log("Destroyed " + gameObject.name);
         }
             
