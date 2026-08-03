ac755aec757a799d646ce0a0903bc6f9f132e9c6
prodws <169688005+prodws@users.noreply.github.com>
2026-08-03 09:53:00 +0200

    Add pushing mechanic and pushable box prefab

---
+++ Assets/Scripts/Pushable.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Pushable : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float pushSpeed = 1.5f;
+
+    private void Awake()
+        {
+            rb = GetComponent<Rigidbody>();
+            rb.constraints =
+                RigidbodyConstraints.FreezeRotationX |
+                RigidbodyConstraints.FreezeRotationZ;
+                }
+    public void Push(Vector3 direction)
+    {
+        Debug.Log("Moving pushable");
+        rb.linearVelocity = direction * pushSpeed;
+    }
+}
