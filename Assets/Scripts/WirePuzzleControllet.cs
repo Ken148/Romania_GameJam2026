c62665cb0634385c99294abb1c430f8628bed633
lautrec93 <lautrec.willson@gmail.com>
2026-08-05 01:55:58 +0300

    puzzle 1 fix

--- Assets/Scripts/WirePuzzleControllet.cs
+++ Assets/Scripts/WirePuzzleControllet.cs
@@ -1,13 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections.Generic;
+using UnityEngine.Rendering;
 using System;
 
 public class WirePuzzleController : MonoBehaviour
 {
     public static WirePuzzleController Instance;
 
-    [Header("Setup")]
     public RectTransform wiresContainer;
     public GameObject insulationPrefab;
     public WireSocket[] leftSockets;
@@ -17,6 +16,14 @@ public class WirePuzzleController : MonoBehaviour
     public Action OnPuzzleSolved;
 
     public MonoBehaviour playerController;
+
+    public Volume blurVolume;
+    public float blurTransitionSpeed = 5f;
+    private float targetWeight = 0f;
+
+    public float insulationInsetFromSocket = 15f;
+
+    public InsulationOverride[] insulationOverrides;
 
     private WireSocket selectedSocket;
     private int connectedCount = 0;
@@ -30,6 +37,8 @@ public class WirePuzzleController : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
         if (playerController != null) playerController.enabled = false;
+
+        targetWeight = 1f;
     }
 
     void OnDisable()
@@ -37,6 +46,17 @@ public class WirePuzzleController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         if (playerController != null) playerController.enabled = true;
+
+        targetWeight = 0f;
+        if (blurVolume != null) blurVolume.weight = 0f;
+    }
+
+    void Update()
+    {
+        if (blurVolume != null && blurVolume.weight != targetWeight)
+        {
+            blurVolume.weight = Mathf.MoveTowards(blurVolume.weight, targetWeight, blurTransitionSpeed * Time.unscaledDeltaTime);
+        }
     }
 
     public void ResetPuzzle()
@@ -107,27 +127,69 @@ public class WirePuzzleController : MonoBehaviour
         }
     }
 
+    InsulationOverride GetOverride(int colorId)
+    {
+        if (insulationOverrides == null) return null;
+        foreach (var o in insulationOverrides)
+            if (o.colorId == colorId && o.useManualTransform) return o;
+        return null;
+    }
+
     void DrawInsulation(WireSocket left, WireSocket right)
     {
         RectTransform line = Instantiate(insulationPrefab, wiresContainer).GetComponent<RectTransform>();
-        Vector3 start = left.GetComponent<RectTransform>().position;
-        Vector3 end = right.GetComponent<RectTransform>().position;
-        Vector3 dir = end - start;
 
-        line.position = start;
-        line.sizeDelta = new Vector2(dir.magnitude, line.sizeDelta.y);
+        RectTransform leftRT = left.GetComponent<RectTransform>();
+        RectTransform rightRT = right.GetComponent<RectTransform>();
+
+        Canvas canvas = wiresContainer.GetComponentInParent<Canvas>();
+        Camera cam = canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : canvas.worldCamera;
+
+        Vector2 startScreen = RectTransformUtility.WorldToScreenPoint(cam, leftRT.position);
+        Vector2 endScreen = RectTransformUtility.WorldToScreenPoint(cam, rightRT.position);
+
+        Vector2 startLocal, endLocal;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(wiresContainer, startScreen, cam, out startLocal);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(wiresContainer, endScreen, cam, out endLocal);
+
+        Vector2 dir = endLocal - startLocal;
+        float fullLength = dir.magnitude;
+        Vector2 dirNormalized = fullLength > 0.001f ? dir.normalized : Vector2.right;
+
+        Vector2 adjustedStart = startLocal + dirNormalized * insulationInsetFromSocket;
+        float adjustedLength = fullLength - insulationInsetFromSocket * 2f;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        line.rotation = Quaternion.Euler(0, 0, angle);
+
+        var ov = GetOverride(left.colorId);
+        if (ov != null)
+        {
+            adjustedStart += ov.manualOffset;
+            angle += ov.manualAngleOffset;
+            adjustedLength *= ov.manualScale;
+        }
+
+        line.anchoredPosition = adjustedStart;
+        line.sizeDelta = new Vector2(adjustedLength, line.sizeDelta.y);
+        line.localRotation = Quaternion.Euler(0, 0, angle);
 
         Image img = line.GetComponent<Image>();
         if (img != null && wireColors != null && wireColors.Length > left.colorId)
             img.color = wireColors[left.colorId];
     }
 
+    [ContextMenu("TEST: Show Puzzle")]
     public void TestShowPuzzle()
     {
         gameObject.SetActive(true);
     }
+}
 
-
+[System.Serializable]
+public class InsulationOverride
+{
+    public int colorId;
+    public bool useManualTransform = false;
+    public Vector2 manualOffset;
+    public float manualAngleOffset;
+    public float manualScale = 1f;
 }
