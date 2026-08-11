ac8700722cee20bd1867ae0cdd60aa482c7c24af
Ken148 <ken.turk70@gmail.com>
2026-08-11 14:06:28 +0200

    fixed water bug

--- Assets/Scripts/Pushable.cs
+++ Assets/Scripts/Pushable.cs
@@ -6,7 +6,12 @@ public class Pushable : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float pushSpeed = 1.5f;
 
+    // How long the object can go without receiving
+    // another Push() call before the push is considered finished.
+    [SerializeField] private float pushTimeout = 0.1f;
+
     private bool isBeingPushed = false;
+    private float lastPushTime;
 
     private void Awake()
     {
@@ -14,9 +19,23 @@ public class Pushable : MonoBehaviour
         rb.constraints = RigidbodyConstraints.FreezeRotation;
     }
 
+    private void Update()
+    {
+        // If Push() has not been called recently,
+        // consider the pushing action finished.
+        if (isBeingPushed &&
+            Time.time - lastPushTime > pushTimeout)
+        {
+            StopPush();
+        }
+    }
+
     public void Push(Vector3 direction)
     {
         rb.linearVelocity = direction * pushSpeed;
+
+        // Remember when Push() was last called.
+        lastPushTime = Time.time;
 
         if (!isBeingPushed)
         {
@@ -37,6 +56,21 @@ public class Pushable : MonoBehaviour
 
             // Stop ONLY the object/box water sound.
             AudioManager.Instance.StopObjectWaterPush();
+        }
+    }
+
+    private void OnDisable()
+    {
+        // Safety: never leave the water sound playing
+        // if this object is disabled or destroyed.
+        if (isBeingPushed)
+        {
+            isBeingPushed = false;
+
+            if (AudioManager.Instance != null)
+            {
+                AudioManager.Instance.StopObjectWaterPush();
+            }
         }
     }
 }
